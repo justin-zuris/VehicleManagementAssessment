2296415b0b91cfe0aa27fdb3c29a74f09c1e7631
justin-zuris <justin.greenwood@zuristechnology.com>
2016-12-12 15:06:35 -0500

    .

--- UniFirst.VehicleManagement/Model/Vehicle.cs
+++ UniFirst.VehicleManagement/Model/Vehicle.cs
@@ -16,7 +16,7 @@ namespace UniFirst.VehicleManagement.Model
         [Required(ErrorMessage = "Model is required")]
         public string Model { get; set; }
 
-        [Required(ErrorMessage = "Year is required"), RegularExpression(pattern: @"^\d{3}$", ErrorMessage = "Four digit year")]
+        [Required(ErrorMessage = "Year is required"), RegularExpression(pattern: @"^\d{4}$", ErrorMessage = "Four digit year")]
         public string Year { get; set; }
 
         [Required(ErrorMessage = "VIN is required"), VINValidation]
