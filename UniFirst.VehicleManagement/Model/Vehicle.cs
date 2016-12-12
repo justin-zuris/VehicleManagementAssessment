b0ed8312afc143bc138039bb8eb8f803d68dfa03
justin-zuris <justin.greenwood@zuristechnology.com>
2016-12-12 15:06:05 -0500

    forgot to fix stupid regex

--- UniFirst.VehicleManagement/Model/Vehicle.cs
+++ UniFirst.VehicleManagement/Model/Vehicle.cs
@@ -16,7 +16,7 @@ namespace UniFirst.VehicleManagement.Model
         [Required(ErrorMessage = "Model is required")]
         public string Model { get; set; }
 
-        [Required(ErrorMessage = "Year is required"), RegularExpression(pattern: @"\d\d\d\d", ErrorMessage = "Four digit year")]
+        [Required(ErrorMessage = "Year is required"), RegularExpression(pattern: @"^\d{3}$", ErrorMessage = "Four digit year")]
         public string Year { get; set; }
 
         [Required(ErrorMessage = "VIN is required"), VINValidation]
